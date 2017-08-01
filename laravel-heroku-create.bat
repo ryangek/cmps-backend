6298f327003d905eff6af5f0d34e2b9993c43ada
RyanGek <artistkanjai1@gmail.com>
2017-08-02 03:01:32 +0700

    laravel-heroku

--- laravel-heroku-create.bat
+++ laravel-heroku-create.bat
@@ -3,8 +3,8 @@ pause
 echo ######################################
 echo ## Add addon "Postgresql" on Heroku ##
 echo ######################################
+set /p app_name=<app_name.txt
 heroku apps:create %app_name%
 heroku addons:create heroku-postgresql:hobby-dev --app %app_name%
 heroku git:remote --app $app_name%
-start call laravel-heroku-app.bat app_name %app_name%
 exit
--- laravel-heroku-git.bat
+++ laravel-heroku-git.bat
@@ -3,6 +3,7 @@ pause
 echo #################################
 echo ## DO upload project to Heroku ##
 echo #################################
+set /p app_name=<app_name.txt
 git add .
 git commit -am "%app_name%"
 git push heroku master
