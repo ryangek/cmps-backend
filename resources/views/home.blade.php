d83777e3b61c28bcaa9688fffaf6b15f842a3c2e
ryangek <artistkanjai1@gmail.com>
2017-08-27 06:17:34 +0700

    carparkdev

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    {{ print_r($user) }}
+                    {{ dd($user) }}
                 </div>
             </div>
         </div>
