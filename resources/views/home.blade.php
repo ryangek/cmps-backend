0472e9815660331985692268ffde326c452bf092
ryangek <artistkanjai1@gmail.com>
2017-08-27 05:40:47 +0700

    carparkdev

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    {{ print_r($user) }}
                 </div>
             </div>
         </div>
