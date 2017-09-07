79ef99f445bfe577f905202caa4a467723b0bee5
ryangek <artistkanjai1@gmail.com>
2017-09-07 12:39:40 +0700

    BBB

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Car Park API
                 </div>
 
                 <div class="links">
